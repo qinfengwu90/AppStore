885deb29242b93e861b7facb0100142008ab73e2
Qinfeng Wu <qinfengwu90@gmail.com>
2022-12-21 21:25:45 +0000

    initial commit

---
+++ backend/elasticsearch.go
@@ -0,0 +1,81 @@
+package backend
+
+import (
+	"context"
+	"fmt"
+
+	"appstore/constants"
+
+	"github.com/olivere/elastic/v7"
+)
+
+var (
+	ESBackend *ElasticsearchBackend
+)
+
+type ElasticsearchBackend struct {
+	client *elastic.Client
+}
+
+func (backend ElasticsearchBackend) ReadFromES() { // the parenthesis before the function name is the receiver
+
+}
+
+func InitElasticsearchBackend() {
+	// new ElasticsearchBackend object to be used for ES operation
+	client, err := elastic.NewClient(
+		elastic.SetURL(constants.ES_URL),
+		elastic.SetBasicAuth(constants.ES_USERNAME, constants.ES_PASSWORD))
+	if err != nil {
+		panic(err)
+	}
+
+	exists, err := client.IndexExists(constants.APP_INDEX).Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	if !exists {
+		mapping := `{
+				"mappings": {
+					"properties": {
+						"id":       { "type": "keyword" },
+						"user":     { "type": "keyword" },
+						"title":      { "type": "text"},
+						"description":  { "type": "text" },
+						"price":      { "type": "keyword", "index": false },
+						"url":     { "type": "keyword", "index": false }
+					}
+				}
+			}`
+		_, err := client.CreateIndex(constants.APP_INDEX).Body(mapping).Do(context.Background())
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	exists, err = client.IndexExists(constants.USER_INDEX).Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	if !exists {
+		mapping := `{
+						 "mappings": {
+							 "properties": {
+								"username": {"type": "keyword"},
+								"password": {"type": "keyword"},
+								"age": {"type": "long", "index": false},
+								"gender": {"type": "keyword", "index": false}
+							 }
+						}
+					}`
+		_, err = client.CreateIndex(constants.USER_INDEX).Body(mapping).Do(context.Background())
+		if err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println("Indexes are created.")
+
+	ESBackend = &ElasticsearchBackend{client: client}
+}
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"appstore/backend"
+	"appstore/handler"
+)
+
+func main() {
+	fmt.Println("started-service")
+	backend.InitElasticsearchBackend()
+	log.Fatal(http.ListenAndServe(":8080", handler.InitRouter()))
+}
